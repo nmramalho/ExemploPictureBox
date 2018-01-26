107a266fa3aef74537e6c0fb77f4129a15496b6a
Nuno Ramalho <profnunoramalho@gmail.com>
2018-01-26 12:57:04 +0000

    Add project files.

---
+++ ExemploPictureBox/FormPrincipal.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ExemploPictureBox
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCarregarImagens = new System.Windows.Forms.Button();
+            this.ButtonLimpar = new System.Windows.Forms.Button();
+            this.pictureBoxQUATRO = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTRES = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDOIS = new System.Windows.Forms.PictureBox();
+            this.pictureBoxUM = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQUATRO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTRES)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDOIS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonCarregarImagens
+            // 
+            this.ButtonCarregarImagens.Location = new System.Drawing.Point(468, 12);
+            this.ButtonCarregarImagens.Name = "ButtonCarregarImagens";
+            this.ButtonCarregarImagens.Size = new System.Drawing.Size(94, 34);
+            this.ButtonCarregarImagens.TabIndex = 4;
+            this.ButtonCarregarImagens.Text = "Carregar Imagens";
+            this.ButtonCarregarImagens.UseVisualStyleBackColor = true;
+            this.ButtonCarregarImagens.Click += new System.EventHandler(this.ButtonCarregarImagens_Click);
+            // 
+            // ButtonLimpar
+            // 
+            this.ButtonLimpar.Location = new System.Drawing.Point(608, 12);
+            this.ButtonLimpar.Name = "ButtonLimpar";
+            this.ButtonLimpar.Size = new System.Drawing.Size(94, 34);
+            this.ButtonLimpar.TabIndex = 5;
+            this.ButtonLimpar.Text = "Limpar";
+            this.ButtonLimpar.UseVisualStyleBackColor = true;
+            this.ButtonLimpar.Click += new System.EventHandler(this.ButtonLimpar_Click);
+            // 
+            // pictureBoxQUATRO
+            // 
+            this.pictureBoxQUATRO.Image = global::ExemploPictureBox.Properties.Resources.quatro;
+            this.pictureBoxQUATRO.Location = new System.Drawing.Point(278, 12);
+            this.pictureBoxQUATRO.Name = "pictureBoxQUATRO";
+            this.pictureBoxQUATRO.Size = new System.Drawing.Size(47, 34);
+            this.pictureBoxQUATRO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxQUATRO.TabIndex = 9;
+            this.pictureBoxQUATRO.TabStop = false;
+            // 
+            // pictureBoxTRES
+            // 
+            this.pictureBoxTRES.Image = global::ExemploPictureBox.Properties.Resources.tres;
+            this.pictureBoxTRES.Location = new System.Drawing.Point(220, 12);
+            this.pictureBoxTRES.Name = "pictureBoxTRES";
+            this.pictureBoxTRES.Size = new System.Drawing.Size(47, 34);
+            this.pictureBoxTRES.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxTRES.TabIndex = 8;
+            this.pictureBoxTRES.TabStop = false;
+            // 
+            // pictureBoxDOIS
+            // 
+            this.pictureBoxDOIS.Image = global::ExemploPictureBox.Properties.Resources.dois;
+            this.pictureBoxDOIS.Location = new System.Drawing.Point(158, 12);
+            this.pictureBoxDOIS.Name = "pictureBoxDOIS";
+            this.pictureBoxDOIS.Size = new System.Drawing.Size(47, 34);
+            this.pictureBoxDOIS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDOIS.TabIndex = 7;
+            this.pictureBoxDOIS.TabStop = false;
+            // 
+            // pictureBoxUM
+            // 
+            this.pictureBoxUM.Image = global::ExemploPictureBox.Properties.Resources.um;
+            this.pictureBoxUM.Location = new System.Drawing.Point(91, 12);
+            this.pictureBoxUM.Name = "pictureBoxUM";
+            this.pictureBoxUM.Size = new System.Drawing.Size(47, 34);
+            this.pictureBoxUM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxUM.TabIndex = 6;
+            this.pictureBoxUM.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(468, 323);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(91, 323);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(468, 87);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(91, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBoxQUATRO);
+            this.Controls.Add(this.pictureBoxTRES);
+            this.Controls.Add(this.pictureBoxDOIS);
+            this.Controls.Add(this.pictureBoxUM);
+            this.Controls.Add(this.ButtonLimpar);
+            this.Controls.Add(this.ButtonCarregarImagens);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormPrincipal";
+            this.Text = "Exemplo PictureBox";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQUATRO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTRES)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDOIS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button ButtonCarregarImagens;
+        private System.Windows.Forms.Button ButtonLimpar;
+        private System.Windows.Forms.PictureBox pictureBoxUM;
+        private System.Windows.Forms.PictureBox pictureBoxDOIS;
+        private System.Windows.Forms.PictureBox pictureBoxTRES;
+        private System.Windows.Forms.PictureBox pictureBoxQUATRO;
+    }
+}
+
---
+++ ExemploPictureBox/FormPrincipal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploPictureBox
+{
+    public partial class FormPrincipal : Form
+    {
+        private string[] imagens = { "um", "dois", "tres", "quatro" };
+        private PictureBox[] pictureBoxs;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+
+            pictureBoxs = new PictureBox[4];
+
+            pictureBoxs[0] = pictureBox1;
+            pictureBoxs[1] = pictureBox2;
+            pictureBoxs[2] = pictureBox3;
+            pictureBoxs[3] = pictureBox4;
+
+
+        }
+
+        private void ButtonCarregarImagens_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                CarregaImagem(pictureBoxs[i], imagens[i]);
+            }
+
+        }
+
+        private void ButtonLimpar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                LimpaImagens(pictureBoxs[i], imagens[i]);
+            }
+
+
+        }
+
+        private void LimpaImagens(PictureBox pic, string v)
+        {
+            pic.Image = null;
+        }
+
+        private void CarregaImagem(PictureBox pic, string nome)
+        {
+            switch (nome)
+            {
+                case "um":
+                    pic.Image = Properties.Resources.um;
+                    break;
+
+                case "dois":
+                    pic.Image = Properties.Resources.dois;
+                    break;
+
+                case "tres":
+                    pic.Image = Properties.Resources.tres;
+                    break;
+
+                case "quatro":
+                    pic.Image = Properties.Resources.quatro;
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
