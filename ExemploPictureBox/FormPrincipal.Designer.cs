5bb3c1e525e9aacef4030181c303f3977052842f
Nuno Ramalho <profnunoramalho@gmail.com>
2018-01-27 21:57:42 +0000

    Atualização para permitir "drag and drop"

--- ExemploPictureBox/FormPrincipal.Designer.cs
+++ ExemploPictureBox/FormPrincipal.Designer.cs
@@ -38,6 +38,7 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxInstrucoes = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQUATRO)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTRES)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDOIS)).BeginInit();
@@ -50,9 +51,9 @@
             // 
             // ButtonCarregarImagens
             // 
-            this.ButtonCarregarImagens.Location = new System.Drawing.Point(468, 12);
+            this.ButtonCarregarImagens.Location = new System.Drawing.Point(265, 12);
             this.ButtonCarregarImagens.Name = "ButtonCarregarImagens";
-            this.ButtonCarregarImagens.Size = new System.Drawing.Size(94, 34);
+            this.ButtonCarregarImagens.Size = new System.Drawing.Size(149, 34);
             this.ButtonCarregarImagens.TabIndex = 4;
             this.ButtonCarregarImagens.Text = "Carregar Imagens";
             this.ButtonCarregarImagens.UseVisualStyleBackColor = true;
@@ -60,7 +61,7 @@
             // 
             // ButtonLimpar
             // 
-            this.ButtonLimpar.Location = new System.Drawing.Point(608, 12);
+            this.ButtonLimpar.Location = new System.Drawing.Point(420, 12);
             this.ButtonLimpar.Name = "ButtonLimpar";
             this.ButtonLimpar.Size = new System.Drawing.Size(94, 34);
             this.ButtonLimpar.TabIndex = 5;
@@ -70,81 +71,115 @@
             // 
             // pictureBoxQUATRO
             // 
+            this.pictureBoxQUATRO.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxQUATRO.Image = global::ExemploPictureBox.Properties.Resources.quatro;
-            this.pictureBoxQUATRO.Location = new System.Drawing.Point(278, 12);
+            this.pictureBoxQUATRO.Location = new System.Drawing.Point(197, 12);
             this.pictureBoxQUATRO.Name = "pictureBoxQUATRO";
             this.pictureBoxQUATRO.Size = new System.Drawing.Size(47, 34);
             this.pictureBoxQUATRO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxQUATRO.TabIndex = 9;
             this.pictureBoxQUATRO.TabStop = false;
+            this.pictureBoxQUATRO.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxQUATRO_MouseDown);
             // 
             // pictureBoxTRES
             // 
+            this.pictureBoxTRES.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxTRES.Image = global::ExemploPictureBox.Properties.Resources.tres;
-            this.pictureBoxTRES.Location = new System.Drawing.Point(220, 12);
+            this.pictureBoxTRES.Location = new System.Drawing.Point(139, 12);
             this.pictureBoxTRES.Name = "pictureBoxTRES";
             this.pictureBoxTRES.Size = new System.Drawing.Size(47, 34);
             this.pictureBoxTRES.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxTRES.TabIndex = 8;
             this.pictureBoxTRES.TabStop = false;
+            this.pictureBoxTRES.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxTRES_MouseDown);
             // 
             // pictureBoxDOIS
             // 
+            this.pictureBoxDOIS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxDOIS.Image = global::ExemploPictureBox.Properties.Resources.dois;
-            this.pictureBoxDOIS.Location = new System.Drawing.Point(158, 12);
+            this.pictureBoxDOIS.Location = new System.Drawing.Point(75, 12);
             this.pictureBoxDOIS.Name = "pictureBoxDOIS";
             this.pictureBoxDOIS.Size = new System.Drawing.Size(47, 34);
             this.pictureBoxDOIS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxDOIS.TabIndex = 7;
             this.pictureBoxDOIS.TabStop = false;
+            this.pictureBoxDOIS.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxDOIS_MouseDown);
             // 
             // pictureBoxUM
             // 
+            this.pictureBoxUM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxUM.Image = global::ExemploPictureBox.Properties.Resources.um;
-            this.pictureBoxUM.Location = new System.Drawing.Point(91, 12);
+            this.pictureBoxUM.Location = new System.Drawing.Point(12, 12);
             this.pictureBoxUM.Name = "pictureBoxUM";
             this.pictureBoxUM.Size = new System.Drawing.Size(47, 34);
             this.pictureBoxUM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxUM.TabIndex = 6;
             this.pictureBoxUM.TabStop = false;
+            this.pictureBoxUM.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxUM_MouseDown);
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(468, 323);
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(395, 74);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox4.Size = new System.Drawing.Size(122, 93);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox4_DragDrop);
+            this.pictureBox4.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox4_DragEnter);
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(91, 323);
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(267, 74);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox3.Size = new System.Drawing.Size(122, 93);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox3_DragDrop);
+            this.pictureBox3.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox3_DragEnter);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(468, 87);
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(139, 74);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox2.Size = new System.Drawing.Size(122, 93);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox2_DragDrop);
+            this.pictureBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox2_DragEnter);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(91, 87);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 74);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 194);
+            this.pictureBox1.Size = new System.Drawing.Size(120, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            // 
+            // textBoxInstrucoes
+            // 
+            this.textBoxInstrucoes.Location = new System.Drawing.Point(12, 183);
+            this.textBoxInstrucoes.Multiline = true;
+            this.textBoxInstrucoes.Name = "textBoxInstrucoes";
+            this.textBoxInstrucoes.ReadOnly = true;
+            this.textBoxInstrucoes.Size = new System.Drawing.Size(505, 105);
+            this.textBoxInstrucoes.TabIndex = 10;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(532, 301);
+            this.Controls.Add(this.textBoxInstrucoes);
             this.Controls.Add(this.pictureBoxQUATRO);
             this.Controls.Add(this.pictureBoxTRES);
             this.Controls.Add(this.pictureBoxDOIS);
@@ -157,6 +192,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "FormPrincipal";
             this.Text = "Exemplo PictureBox";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQUATRO)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTRES)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDOIS)).EndInit();
@@ -166,6 +202,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -181,6 +218,7 @@
         private System.Windows.Forms.PictureBox pictureBoxDOIS;
         private System.Windows.Forms.PictureBox pictureBoxTRES;
         private System.Windows.Forms.PictureBox pictureBoxQUATRO;
+        private System.Windows.Forms.TextBox textBoxInstrucoes;
     }
 }
 
--- ExemploPictureBox/FormPrincipal.cs
+++ ExemploPictureBox/FormPrincipal.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ExemploPictureBox
@@ -18,14 +12,27 @@ namespace ExemploPictureBox
         public FormPrincipal()
         {
             InitializeComponent();
+        }
 
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
             pictureBoxs = new PictureBox[4];
+
+            // ativar a operação Drop (largar) nas PictureBox
+            pictureBox1.AllowDrop = true;
+            pictureBox2.AllowDrop = true;
+            pictureBox3.AllowDrop = true;
+            pictureBox4.AllowDrop = true;
 
             pictureBoxs[0] = pictureBox1;
             pictureBoxs[1] = pictureBox2;
             pictureBoxs[2] = pictureBox3;
             pictureBoxs[3] = pictureBox4;
 
+            textBoxInstrucoes.Text = " Objetivo da aplicação:\r\n\r\n  --> Agarrar e largar (Drag and Drop) a imagens dos números nas PictureBoxes maiores " +
+                "\r\n\r\n  --> Carregar as imagens automaticamente nas PictureBoxes maiores de acordo com os nomes " +
+                " encontrados num array de strings, por exemplo  string[] imagens = { \"um\", \"dois\", \"tres\",\"quatro\" };";
 
         }
 
@@ -78,5 +85,69 @@ namespace ExemploPictureBox
             }
 
         }
+
+
+        private void pictureBoxUM_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBoxUM.DoDragDrop(pictureBoxUM.Image, DragDropEffects.Copy);
+        }
+
+        private void pictureBoxDOIS_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBoxDOIS.DoDragDrop(pictureBoxDOIS.Image, DragDropEffects.Copy);
+        }
+
+        private void pictureBoxTRES_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBoxTRES.DoDragDrop(pictureBoxTRES.Image, DragDropEffects.Copy);
+        }
+
+        private void pictureBoxQUATRO_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBoxQUATRO.DoDragDrop(pictureBoxQUATRO.Image, DragDropEffects.Copy);
+        }
+
+
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+        private void pictureBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+        private void pictureBox3_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+
+        private void pictureBox4_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.AllowedEffect;
+        }
+
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox1.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+        private void pictureBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox2.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+        private void pictureBox3_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox3.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+        private void pictureBox4_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox4.Image = (Bitmap)e.Data.GetData(DataFormats.Bitmap);
+        }
+
+
     }
 }
